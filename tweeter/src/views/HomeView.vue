324a51688b70b7f7312785ec444e736f8b1af876
rusafae <rusafae@D350-S15>
2024-03-25 10:23:14 +0100

    fgdfhdhg

--- tweeter/src/views/HomeView.vue
+++ tweeter/src/views/HomeView.vue
@@ -4,6 +4,28 @@ import TheWelcome from '../components/TheWelcome.vue'
 
 <template>
   <main>
-    <TheWelcome />
+    <div id="ContainerGeneral">
+      <img src="../../public/logoX.png" alt="">
+      <div>
+        <h1>Ça se passe maintenant</h1>
+        <h2>Inscrivez vous</h2>
+        <form>
+          <label for=""></label>
+          <input type="text">
+        </form>
+      </div>
+    </div>
   </main>
 </template>
+
+
+<style scoped>
+
+#ContainerGeneral{
+  display: flex;
+  flex-direction: row;
+}
+
+
+
+</style>
